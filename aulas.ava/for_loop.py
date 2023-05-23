457f92a227497eb4364e91e188d4133feb93d319
Isis Paiva <sousaisis2004@gmail.com>
2023-05-23 16:30:12 -0300

    exercicios de laços e while loops!

---
+++ aulas.ava/for_loop.py
@@ -0,0 +1,8 @@
+soma =  0
+qtd = 0
+for i in range(1,101):
+    if(i%2==0):
+        soma += i
+        qtd += 1
+media = soma/qtd
+print('a  média dos pares de 1 até 100 é {}'.format(media))
---
+++ aulas.ava/tabuada_usando_laços.py
@@ -0,0 +1,23 @@
+#tabuada de 2 while
+tabuada = 1
+while(tabuada <= 10):
+    print('tabuada do {}:'.format(tabuada))
+    i=1
+    while(i<=10):
+        print('{} x {} = {}'.format(tabuada,i,(tabuada*i)))
+        i += 1
+    tabuada += 1
+################################################################################################
+tabuada de 2 for
+for i in range(1,11):
+    print('tabuada do {}:'.format(i))
+    for j in range(1, 11):
+        print('{} x {} = {}'.format(i,j,(i*j)))
+################################################################################################
+#tabuada while e for
+tabuada = 1
+while(tabuada <=10):
+    print('tabuada do {}'.format(tabuada))
+    for i in range(1,11):
+        print('{} x {} = {}'.format(tabuada,i,(tabuada*i)))
+    tabuada +=1
